46232b4a5d5742bede4220c6d69ae686d696b824
PinhasZiv <63521561+PinhasZiv@users.noreply.github.com>
2022-04-08 18:57:34 +0300

    updated file after apply

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ca-central-1"
+}
+
+# PinhasZiv-vpc #
+resource "aws_vpc" "PinhasZiv" {
+  cidr_block = "10.0.0.0/25"
+  tags = {
+    Name = "Pinhas-VPC"
+  }
+}
+
+# Web Subnet #
+resource "aws_subnet" "Web-Tier-Subnet" {
+  vpc_id     = aws_vpc.PinhasZiv.id
+  cidr_block = "10.0.0.0/27"
+
+  tags = {
+    Name = "web-subnet"
+  }
+}
+
+# App Subnet #
+resource "aws_subnet" "App-Tier-Subnet" {
+  vpc_id     = aws_vpc.PinhasZiv.id
+  cidr_block = "10.0.0.32/27"
+
+  tags = {
+    Name = "app-subnet"
+  }
+}
+
+# DB Subnet #
+resource "aws_subnet" "DB-Tier-Subnet" {
+  vpc_id     = aws_vpc.PinhasZiv.id
+  cidr_block = "10.0.0.64/28"
+
+  tags = {
+    Name = "db-subnet"
+  }
+}
